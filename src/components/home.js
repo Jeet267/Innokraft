82e5a5751f6454c59fa1a7f5b4eb662a2a5b7ec0
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-12-03 13:37:38 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -10,10 +10,10 @@ const Home = () => {
           backgroundImage: `url(${wall})`,
         }}
       >
-        {/* Content specific to larger screens */}
+
       </div>
 
-      {/* Content for smaller screens */}
+
       <div className="sm:hidden flex items-center  bg-black bg-opacity-50 py-8">
   <h1 className="text-white text-lg font-bold text-center px-4">
     Welcome to Abhyanjana
@@ -21,7 +21,7 @@ const Home = () => {
 </div>
 
 
-      {/* Floating button */}
+
       <div className="absolute top-7 right-4 z-10">
         <button className="bg-yellow-600 text-white font-semibold px-6 py-2 rounded-full shadow-lg hover:bg-yellow-500 transition duration-300">
           Order Now
